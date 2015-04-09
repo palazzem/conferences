3889419b540142fb6d832ddd0ac77a64c82e3682
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-04-09 20:19:08 +0200

    added rackspace notes

--- pycon2015/rackspace/simple_request.py
+++ pycon2015/rackspace/simple_request.py
@@ -3,13 +3,13 @@ import multyvac
 import requests
 
 
-# Rackspace config
+# rackspace configuration
 multyvac.config.api_key = "cloudpipe18" # Rackspace Username
 multyvac.config.api_secret_key = "683df297726d4bb18c6c230e5be795fe" # Rackspace API Key
 multyvac.config.api_url = "https://cloudpipe.tmpnb.org/v1"
 
 
-# Helpers
+# helpers
 def status(url):
     return requests.get(url).status_code
 
@@ -17,6 +17,9 @@ def server_header(url):
     return requests.get(url).headers.get('server')
 
 
-# Execution
+# execution
 jid = multyvac.submit(status, "https://developer.rackspace.com")
 print(multyvac.get(jid).get_result())
+
+# notes about execution
+# multyvac.submit(some_lambda_f, _image="some_docker_image_in_which_launch_the_job")
